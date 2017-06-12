059b3c27d96c0f927bdd59ef9d44b576c081bc25
Max Scher and Rena Rallis <rena@>
2017-06-12 10:56:11 -0700

    add state memory food music inputs

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,12 @@
 //business logic
-function Location(city, country, attraction) {
+function Location(city, state, country, memory, attraction, food, music) {
   this.city = city;
+  this.state = state;
   this.country = country;
+  this.memory = memory;
   this.attraction = attraction;
+  this.food = food;
+  this.music = music;
 }
 
 Location.prototype.destination = function() {
@@ -15,17 +19,26 @@ $(document).ready(function() {
     event.preventDefault();
 
     var inputtedCity = $("input#city").val();
+    var inputtedState = $("input#state").val();
     var inputtedCountry = $("input#country").val();
+    var inputtedMemory = $("input#memory").val();
     var inputtedAttraction = $("input#attraction").val();
-    var newLocation = new Location(inputtedCity, inputtedCountry, inputtedAttraction);
+    var inputtedFood = $("input#food").val();
+    var inputtedMusic = $("input#music").val();
+
+    var newLocation = new Location(inputtedCity, inputtedState, inputtedCountry, inputtedMemory, inputtedAttraction, inputtedFood, inputtedMusic);
 
     $("ul#locations").append("<li><span class='location'>" + newLocation.destination() + "</span></li>");
 
     $(".location").last().click(function() {
         $("#show-location").show();
         $("#show-location h2").text(newLocation.city);
+        $(".state").text(newLocation.state);
         $(".country").text(newLocation.country);
+        $(".memory").text(newLocation.memory);
         $(".attraction").text(newLocation.attraction);
+        $(".food").text(newLocation.food);
+        $(".music").text(newLocation.music);
     });
   });
 });
